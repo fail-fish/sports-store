d0ce2f1510a69c160450d222674356994cd6d07c
Vasyl <vasil.gladkiy@gmail.com>
2020-11-08 14:45:14 +0200

    Add user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,6 @@
+FactoryBot.create(:user,
+                  email: 'store_user@gmail.com',
+                  password: '123456',
+                  password_confirmation: '123456')
+
 FactoryBot.create(:category, :with_products)
