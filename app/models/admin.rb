bd461bd4a2bf4f6d01ff515c22ff490f4c6eda27
Vasyl <vasil.gladkiy@gmail.com>
2020-11-14 22:42:44 +0200

    Add recovarable function for admins

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -24,5 +24,5 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :trackable, :timeoutable
+  devise :database_authenticatable, :trackable, :timeoutable, :recoverable
 end
