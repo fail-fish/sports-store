3a7ad70c40723a83c53753886f425193c0cc452b
Vasyl <vasil.gladkiy@gmail.com>
2020-11-14 11:46:25 +0200

    Add Admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ FactoryBot.create(:user,
                   password_confirmation: '123456')
 
 FactoryBot.create(:admin,
-                    email: 'store_admin@gmail.com',
-                    password: '123456',
-                    password_confirmation: '123456')
+                  email: 'store_admin@gmail.com',
+                  password: '123456',
+                  password_confirmation: '123456')
 
 FactoryBot.create(:category, :with_products)
