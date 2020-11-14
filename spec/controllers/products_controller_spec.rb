b302de2700f6a8fc2c134a246dfeb2e7d2d643b3
Vasyl <vasil.gladkiy@gmail.com>
2020-11-14 11:46:58 +0200

    Fix failing products controller tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
   let!(:product) { create(:product) }
+  let!(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
 
   describe 'GET #index' do
     before do
