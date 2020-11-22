e34d56ff057e97bbce56a688620799f6f663916c
Vasyl <vasil.gladkiy@gmail.com>
2020-11-22 14:33:44 +0200

    Add predefined_caregories

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,12 @@ FactoryBot.create(:category, :with_products)
 Product.first(5).each do |product|
   FactoryBot.create(:order, product: product, user: user)
 end
+
+predefined_caregories = ['Сlothes for Sport',
+                         'Sneakers for Sports',
+                         'Sports Inventory',
+                         'Discounts']
+
+predefined_caregories.each do |predefined_caregory|
+  FactoryBot.create(:category, name: predefined_caregory)
+end
